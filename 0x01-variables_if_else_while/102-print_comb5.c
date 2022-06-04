3f1db4456838a6d94f98c3331f29850244a25b0a
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2022-06-03 18:51:43 -0700

    A program that prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,36 +11,34 @@ int main(void)
 
 	for (a = 48; a <= 57; a++)
 	{
-		for (b = 48; b <= 57; b++)
+	for (b = 48; b <= 57; b++)		
+	{
+	for (c = 48; c <= 57; c++)
+	{
+	for (d = 48; d <= 57; d++)
+	{
+		if (((c + d) > (a + b) && c >= a) || a < c)
 		{
-			for (c = 48; c <= 57; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (((c + d) > (a + b) && c >= a) || a < c)
-					{
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
-
-						if (a + b + c + d == 227 && a == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
+		putchar(a);
+		putchar(b);
+		putchar(' ');
+		putchar(c);
+		putchar(d);
+		
+		if (a + b + c + d == 227 && a == 57)
+		{
+		break;
+		}
+		else
+		{
+		putchar(',');
+		putchar(' ');
+		}
 		}
 	}
-
+	}
+	}
+	}
 	putchar('\n');
-
 	return (0);
 }
