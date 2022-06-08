de1485ac2f9c182775ed8c903967e383f3432a18
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2022-06-08 03:22:02 -0700

    a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,37 +12,38 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)/*First for*/
 		{
-			for (b = 0; b <= n; b++)/*Secondo for*/
+			for (b = 0; b <= n; b++)/*Second for*/
 			{
 				c = a * b;/* multiplication a*b*/
 				if ((c / 10) == 0)/*If the first digit is 0*/
 				{
-					if  (b == 0)
-					{ _putchar ('0');
-					}
-					if (b != 0)/*print if b is different 0*/
-					{
-						_putchar (' ');
-						_putchar (' ');
-						_putchar ((c % 10) + '0');
-					}
+				if  (b == 0)
+				{ 
+				_putchar ('0');
+				}
+				if (b != 0)/*print if b is different 0*/
+				{
+				_putchar (' ');
+				_putchar (' ');
+				_putchar ((c % 10) + '0');
+				}
 				} /*End the first IF*/
 				else if ((c / 10) <= 9 && ((c % 10) <= 9))/*print to 99*/
 				{
-					_putchar (' ');
-					_putchar ((c / 10) + '0');
-					_putchar ((c % 10) + '0');
+				_putchar (' ');
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
 				} /*End ELSE IF*/
 				else/*print from 100*/
 				{
-					_putchar ((c / 100) + '0');
-					_putchar (((c / 10) % 10)  + '0');
-					_putchar ((c % 10) + '0');
+				_putchar ((c / 100) + '0');
+				_putchar (((c / 10) % 10)  + '0');
+				_putchar ((c % 10) + '0');
 				} /*End ELSE*/
 				if (b < n) /*don't print the last (;)*/
 				{
-					_putchar(',');
-					_putchar (' ');
+				_putchar(',');
+				_putchar (' ');
 				}
 			} /*End second FOR*/ _putchar ('\n');
 		} /*End first FOR*/
