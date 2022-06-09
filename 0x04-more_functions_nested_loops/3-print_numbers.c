969bcf91942e614e44ef877f5e05030394fe7a34
Kolade Fatai Opeyemi <adedayo924@gmail.com>
2022-06-09 07:04:38 -0700

    a function that multiplies two integers.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,12 @@
 void print_numbers(void)
 {
 	int i; /*Declaring statements*/
-	
+
 	i = 0;
 	while (i < 10)/*Start While*/
 	{
 		_putchar (i + '0'); /*print value i*/
 		i++;
 	}
-	
 	_putchar ('\n'); /*New line*/
 }
